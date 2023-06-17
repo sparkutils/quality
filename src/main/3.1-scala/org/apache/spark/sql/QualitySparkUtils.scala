916f00ccaad037c149ae368fc5fb4d208d6867a4
Chris Twiner <chris.twiner@gmail.com>
2023-06-17 12:45:48 +0200

    #25 - have to use builtIn to register functions for views - versions
    (cherry picked from commit 9b1d77590c12941a8146d82161823c658aaa72d0)

--- src/main/3.1-scala/org/apache/spark/sql/QualitySparkUtils.scala
+++ src/main/3.1-scala/org/apache/spark/sql/QualitySparkUtils.scala
@@ -308,13 +308,14 @@ object QualitySparkUtils {
    */
   def registerFunctionViaExtension(extensions: SparkSessionExtensions)(name: String, builder: Seq[Expression] => Expression) =
     extensions.injectFunction( (FunctionIdentifier(name), new ExpressionInfo(name, name) , builder) )
+
   /**
    * Used by the SparkSessionExtensions mechanism but registered via builtin registry
    * @param name
    * @param builder
    */
   def registerFunctionViaBuiltin(name: String, builder: Seq[Expression] => Expression) =
-    FunctionRegistry.builtin.internalRegisterFunction( FunctionIdentifier(name), new ExpressionInfo(name, name) , builder)
+    FunctionRegistry.builtin.registerFunction( FunctionIdentifier(name), new ExpressionInfo(name, name) , builder)
 
   /**
    * Type signature changed for 3.4 to more detailed setup, 12.2 already uses it
