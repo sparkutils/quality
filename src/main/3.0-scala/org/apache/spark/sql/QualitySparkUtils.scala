195011b79457a580ba9a729ede6f6b8a1d955871
Chris Twiner <chris.twiner@gmail.com>
2024-02-20 17:54:56 +0100

    #58 - shims for 3.0, 3.1

--- src/main/3.0-scala/org/apache/spark/sql/QualitySparkUtils.scala
+++ src/main/3.0-scala/org/apache/spark/sql/QualitySparkUtils.scala
@@ -1,41 +1,21 @@
 package org.apache.spark.sql
 
-import java.util.Locale
-
-import com.sparkutils.quality.impl.{RuleEngineRunner, RuleFolderRunner, RuleRunner, ShowParams}
 import com.sparkutils.quality.impl.util.DebugTime.debugTime
 import com.sparkutils.quality.impl.util.PassThrough
-import org.apache.spark.sql.catalyst.{CatalystTypeConverters, FunctionIdentifier}
-import org.apache.spark.sql.catalyst.analysis.{Analyzer, FunctionRegistry, ResolveCreateNamedStruct, ResolveHigherOrderFunctions, ResolveInlineTables, ResolveLambdaVariables, ResolveTimeZone, TypeCheckResult, TypeCoercion, UnresolvedFunction, UnresolvedRelation}
+import com.sparkutils.quality.impl.{RuleEngineRunner, RuleFolderRunner, RuleRunner}
+import org.apache.spark.sql.catalyst.analysis.{Analyzer, ResolveCreateNamedStruct, ResolveHigherOrderFunctions, ResolveInlineTables, ResolveLambdaVariables, ResolveTimeZone, TypeCoercion}
 import org.apache.spark.sql.catalyst.catalog.SessionCatalog
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.catalyst.errors.TreeNodeException
-import org.apache.spark.sql.catalyst.expressions.{Add, Alias, Attribute, BindReferences, Cast, EqualNullSafe, Expression, ExpressionInfo, GetArrayStructFields, GetStructField, Literal, PrettyAttribute}
+import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, BindReferences, EqualNullSafe, Expression, Literal}
 import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, UnaryNode}
 import org.apache.spark.sql.catalyst.rules.Rule
-import org.apache.spark.sql.catalyst.util.{toPrettySQL, usePrettyExpression}
-import org.apache.spark.sql.execution.SparkSqlParser
 import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.qualityFunctions.{Digest, InterpretedHashLongsFunction}
-import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.{CalendarInterval, UTF8String}
 import org.apache.spark.util.Utils
 
 /**
  * Set of utilities to reach in to private functions
  */
 object QualitySparkUtils {
-
-  implicit class UnresolvedFunctionOps(unresolvedFunction: UnresolvedFunction) {
-
-    def theArguments: Seq[Expression] =
-      unresolvedFunction.arguments
-
-    def withArguments(children: Seq[Expression]): UnresolvedFunction =
-      unresolvedFunction.copy(arguments = children)
-  }
-
-  def isPrimitive(dataType: DataType) = CatalystTypeConverters.isPrimitive(dataType)
 
   /**
    * Where resolveWith is not possible (e.g. 10.x DBRs) it is disabled here.
@@ -45,44 +25,6 @@ object QualitySparkUtils {
    */
   def resolveWithOverride(orig: Option[DataFrame]): Option[DataFrame] =
     orig
-
-  /**
-   * Dbr 11.2 broke the contract for add and cast
-   * @param left
-   * @param right
-   * @return
-   */
-  def add(left: Expression, right: Expression, dataType: DataType): Expression =
-    Add(left, right)
-
-  /**
-   * Dbr 11.2 broke the contract for add and cast
-   * @param child
-   * @param dataType
-   * @return
-   */
-  def cast(child: Expression, dataType: DataType): Expression =
-    Cast(child, dataType)
-
-  /**
-   * Arguments for everything above 2.4
-   */
-  def arguments(unresolvedFunction: UnresolvedFunction): Seq[Expression] =
-    unresolvedFunction.arguments
-
-  /**
-   * Provides Spark 3 specific version of hashing CalendarInterval
-   * @param c
-   * @param hashlongs
-   * @param digest
-   * @return
-   */
-  def hashCalendarInterval(c: CalendarInterval, hashlongs: InterpretedHashLongsFunction, digest: Digest): Digest = {
-    import hashlongs._
-    hashInt(c.months, hashInt(
-      c.days
-      , hashLong(c.microseconds, digest)))
-  }
 
   /**
    * Resolves expressions against a dataframe, this allows them to be swapped out after name checking - spark cannot then
@@ -270,124 +212,4 @@ object QualitySparkUtils {
 
     override lazy val validConstraints: Set[Expression] = mygetAllValidConstraints(Seq(expr))
   }
-
-  /**
-   * Creates a new parser, introduced in 0.4 - 3.2.0 due to SparkSqlParser having no params
-   * @return
-   */
-  def newParser() = {
-    new SparkSqlParser(new SQLConf)
-  }
-
-  /**
-   * Registers functions with spark, Introduced in 0.4 - 3.2.0 support due to extra source parameter - "built-in" is used as no other option is remotely close
-   * @param funcReg
-   * @param name
-   * @param builder
-   */
-  def registerFunction(funcReg: FunctionRegistry)(name: String, builder: Seq[Expression] => Expression) =
-    funcReg.createOrReplaceTempFunction(name, builder)
-
-  def toString(dataFrame: DataFrame, showParams: ShowParams = ShowParams()) =
-    dataFrame.showString(showParams.numRows, showParams.truncate, showParams.vertical)
-
-  /**
-   * Used by the SparkSessionExtensions mechanism
-   * @param extensions
-   * @param name
-   * @param builder
-   */
-  def registerFunctionViaExtension(extensions: SparkSessionExtensions)(name: String, builder: Seq[Expression] => Expression) =
-    extensions.injectFunction( (FunctionIdentifier(name), new ExpressionInfo(name, name) , builder) )
-
-  /**
-   * Used by the SparkSessionExtensions mechanism but registered via builtin registry
-   * @param name
-   * @param builder
-   */
-  def registerFunctionViaBuiltin(name: String, builder: Seq[Expression] => Expression) =
-    FunctionRegistry.builtin.registerFunction( FunctionIdentifier(name), new ExpressionInfo(name, name) , builder)
-
-  /**
-   * Type signature changed for 3.4 to more detailed setup, 12.2 already uses it
-   * @param errorSubClass
-   * @param messageParameters
-   * @return
-   */
-  def mismatch(errorSubClass: String, messageParameters: Map[String, String]): TypeCheckResult =
-    TypeCheckResult.TypeCheckFailure(s"$errorSubClass extra info - $messageParameters")
-
-  def toSQLType(t: AbstractDataType): String = t match {
-    case TypeCollection(types) => types.map(toSQLType).mkString("(", " or ", ")")
-    case dt: DataType => quoteByDefault(dt.sql)
-    case at => quoteByDefault(at.simpleString.toUpperCase(Locale.ROOT))
-  }
-  def toSQLExpr(e: Expression): String = {
-    quoteByDefault(toPrettySQL(e))
-  }
-
-  def usePrettyExpression(e: Expression): Expression = e transform {
-    case a: Attribute => new PrettyAttribute(a)
-    case Literal(s: UTF8String, StringType) => PrettyAttribute(s.toString, StringType)
-    case Literal(v, t: NumericType) if v != null => PrettyAttribute(v.toString, t)
-    case Literal(null, dataType) => PrettyAttribute("NULL", dataType)
-    case e: GetStructField =>
-      val name = e.name.getOrElse(e.childSchema(e.ordinal).name)
-      PrettyAttribute(usePrettyExpression(e.child).sql + "." + name, e.dataType)
-    case e: GetArrayStructFields =>
-      PrettyAttribute(usePrettyExpression(e.child) + "." + e.field.name, e.dataType)
-    case c: Cast =>
-      PrettyAttribute(usePrettyExpression(c.child).sql, c.dataType)
-  }
-
-  def toPrettySQL(e: Expression): String = usePrettyExpression(e).sql
-  // Converts an error class parameter to its SQL representation
-  def toSQLValue(v: Any, t: DataType): String = Literal.create(v, t) match {
-    case Literal(null, _) => "NULL"
-    case Literal(v: Float, FloatType) =>
-      if (v.isNaN) "NaN"
-      else if (v.isPosInfinity) "Infinity"
-      else if (v.isNegInfinity) "-Infinity"
-      else v.toString
-    case l @ Literal(v: Double, DoubleType) =>
-      if (v.isNaN) "NaN"
-      else if (v.isPosInfinity) "Infinity"
-      else if (v.isNegInfinity) "-Infinity"
-      else l.sql
-    case l => l.sql
-  }
-
-  private def quoteByDefault(elem: String): String = {
-    "\"" + elem + "\""
-  }
-
-  // https://issues.apache.org/jira/browse/SPARK-43019 in 3.5, backported to 13.1 dbr
-  def sparkOrdering(dataType: DataType): Ordering[_] = dataType.asInstanceOf[AtomicType].ordering
-
-  def tableOrViewNotFound(e: Exception): Option[Either[Exception, Set[String]]] =
-    e match {
-      case ae: AnalysisException =>
-        Some(ae.plan.fold[Either[Exception, Set[String]]]{
-          // spark 2.4 just has exception: Table or view not found: names
-          if (ae.message.contains("Table or view not found"))
-            Right(Set(ae.message.split(":")(1).trim))
-          else
-            Left(ae)
-        } {
-          plan =>
-            val c =
-              plan.collect {
-                case ur: UnresolvedRelation =>
-                  ur.tableName
-              }
-
-            if (c.isEmpty)
-              Left(ae) // not what we expected
-            else
-              Right(c.toSet)
-        })
-      case _ => None
-    }
-
-  def rowEncoder(structType: StructType) = RowEncoder(structType)
 }
