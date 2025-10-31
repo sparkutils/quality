917fb7e9f52d1873cd363ce4016faf7e485e7cf6
georgios <george.orfanidi@gmail.com>
2025-10-31 14:52:35 +0100

    use the last working JdbcHelper version

--- src/main/scala/org/apache/spark/sql/qualityFunctions/jdbc/JdbcHelper.scala
+++ src/main/scala/org/apache/spark/sql/qualityFunctions/jdbc/JdbcHelper.scala
@@ -2,23 +2,23 @@ package org.apache.spark.sql.qualityFunctions.jdbc
 
 import com.sparkutils.quality.sparkless.{ProcessorFactory, ProcessorFactoryInputProxyWithState}
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.ShimUtils._
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{GenericRowWithSchema, SpecificInternalRow}
+import org.apache.spark.sql.catalyst.expressions.SpecificInternalRow
 import org.apache.spark.sql.types.{DataType, StructType}
 
 import java.sql.ResultSet
 
 class JdbcHelper private[jdbc](schema: StructType, getters: Seq[(DataType, JDBCValueGetter)]) {
 
-  private def genericRowWithSchema: GenericRowWithSchema = new GenericRowWithSchema(Array.ofDim[Any](schema.size), schema)
+  private def fromRow = expressionEncoder(rowEncoder(schema)).resolveAndBind().createDeserializer()
 
   def wrapResultSet[O](processorFactory: ProcessorFactory[Row, O]): ProcessorFactory[ResultSet, O] = {
     new ProcessorFactoryInputProxyWithState(
       underlyingFactory = processorFactory,
-      stateConstructor = () => (new SpecificInternalRow(getters.map(_._1)), genericRowWithSchema),
+      stateConstructor = () => (new SpecificInternalRow(getters.map(_._1)), fromRow),
       convert = convertResultSetToRow)
   }
-
 
   /**
    * Fills targetRow from resultSet, it does not make any attempt to check if there is a row, callers are responsible for
@@ -33,11 +33,8 @@ class JdbcHelper private[jdbc](schema: StructType, getters: Seq[(DataType, JDBCV
     }
   }
 
-  private def convertResultSetToRow(internalRow: (SpecificInternalRow, GenericRowWithSchema), resultSet: ResultSet): Row = {
+  private def convertResultSetToRow(internalRow: (InternalRow, InternalRow => Row), resultSet: ResultSet): Row = {
     process(internalRow._1, resultSet)
-    schema.indices.foreach{ i =>
-      internalRow._2.values.update(i, internalRow._1.values(i).boxed)
-    }
-    internalRow._2
+    internalRow._2.apply(internalRow._1)
   }
 }
