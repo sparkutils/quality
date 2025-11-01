60a66589bfbabe5e0e281ae0d8c07683cfc22926
Chris Twiner <chris.twiner@gmail.com>
2025-11-01 22:42:26 +0100

    #90 - use new testrunner trait

---
+++ src/test/scala/com/sparkutils/qualityTests/QualityTestRunner.scala
@@ -0,0 +1,11 @@
+package com.sparkutils.qualityTests
+
+import com.sparkutils.testing.TestRunner
+
+object QualityTestRunner extends TestRunner {
+
+  val packageName: String = "com.sparkutils"
+
+  val projectName: String = "Quality"
+
+}
