b1fb1a6c42193e82976dcd44faeea5d23fefcb36
Chris Twiner <chris.twiner@gmail.com>
2025-11-23 15:20:25 +0100

    #100 - probablePass and delta 17.3 test issue ~cut-release

--- src/main/scala/com/sparkutils/quality/impl/RuleImpl.scala
+++ src/main/scala/com/sparkutils/quality/impl/RuleImpl.scala
@@ -5,7 +5,7 @@ import com.sparkutils.quality.impl.ExpressionCompiler.withExpressionCompiler
 import com.sparkutils.quality.impl.util.{Serializing, SubQueryWrapper}
 import com.sparkutils.quality._
 import com.sparkutils.quality.impl.util.Serializing.toSeq
-import org.apache.spark.sql
+
 import org.apache.spark.sql.ShimUtils.newParser
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.analysis.{UnresolvedAttribute, UnresolvedFunction}
@@ -13,9 +13,8 @@ import org.apache.spark.sql.catalyst.expressions.codegen.{CodeAndComment, CodeFo
 import org.apache.spark.sql.catalyst.expressions.{Expression, ScalarSubquery, SubqueryExpression, UnresolvedNamedLambdaVariable, LambdaFunction => SparkLambdaFunction}
 import org.apache.spark.sql.qualityFunctions.{FunN, RefExpressionLazyType}
 import org.apache.spark.sql.types.{DataType, Decimal}
-import org.apache.spark.sql.{ShimUtils, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.unsafe.types.UTF8String
-import org.apache.spark.util.SparkClassUtils
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream, ObjectStreamClass}
 import scala.collection.mutable
