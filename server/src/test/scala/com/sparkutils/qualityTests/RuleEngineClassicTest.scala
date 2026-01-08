ce44d8920188b2205442919d194569217628a314
Chris Twiner <chris.twiner@gmail.com>
2026-01-08 13:20:37 +0100

    #110 - test builds on different OSS versions

--- server/src/test/scala/com/sparkutils/qualityTests/RuleEngineClassicTest.scala
+++ server/src/test/scala/com/sparkutils/qualityTests/RuleEngineClassicTest.scala
@@ -3,7 +3,7 @@ package com.sparkutils.qualityTests
 import com.sparkutils.quality._
 import com.sparkutils.quality.impl.RuleEngineRunner
 import com.sparkutils.quality.impl.extension.FunNRewrite
-import com.sparkutils.qualityTests.util.{ClassicSharedTests, SharedConnectTests}
+import com.sparkutils.qualityTests.util.SharedConnectTests
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.ShimUtils.expression
 import org.apache.spark.sql.functions.col
@@ -98,5 +98,84 @@ class RuleEngineClassicTest extends SharedConnectTests with RuleEngineTestBase {
       }
     }
   }
+// case class Compilation_110(STARTER: String, STARTER_FLAG: String, I1: String, I2: String, I3: String, I4: String)
+
+  val compilationTestData = Seq(
+    Compilation_110("a", "af", Some("a"), "A", "b", None),
+    Compilation_110("a", "af", None, "A", "b", Some("c")),
+    Compilation_110("a", "af", Some("a"), "A", "b", None),
+    Compilation_110("a", "af", None, "A", "b", Some("c")),
+    Compilation_110("a", "af", Some("a"), "A", "b", None)
+  )
+
+  test("Deeply nested projection causes compilation issue") { evalCodeGensNoResolve {
+    import com.sparkutils.quality._
+
+    val spark = sparkSession
+    import spark.implicits._
+
+    import org.apache.spark.sql.functions._
+    import org.apache.spark.sql.types._
+
+    val rule1 = Rule(Id(100, 2), ExpressionRule("I1 IS NOT NULL"), RunOnPassProcessor(1000, Id(1040,1),
+      OutputExpression("array(STARTER, 'EX_1')")))
+    val rule2 = Rule(Id(101, 1), ExpressionRule("I1 IS  NULL "), RunOnPassProcessor(1000, Id(1040,1),
+      OutputExpression("array(STARTER_FLAG, 'EX_2')")))
+    val ruleSuite = RuleSuite(Id(22, 1034), Seq(RuleSet(Id(33, 345), Seq(rule1,rule2))))
+    val schema=DataType.fromDDL("ARRAY<STRING>")
+    val rer :org.apache.spark.sql.Column = ruleEngineRunner(ruleSuite,schema,debugMode = false)
+
+    val fname = outputDir + "/compilation_110_testData"
+
+    compilationTestData.toDS().write.mode("overwrite").parquet(fname)
+
+    val rulesDf = //compilationTestData.toDS()
+      spark.read
+        .parquet(fname)
+
+    val exprString = s"together.result as result"
+    val oneOutDf = rulesDf.select(col("*"), rer.alias("together")).select(col("*"), expr(exprString)).
+      withColumn("TEMP_O_1",col("result").getItem(0)).withColumn("RULE_ID",col("result").getItem(1)).
+      drop("together","result")
+    oneOutDf.head() // should work
+
+    val rule3 = Rule(Id(100, 2), ExpressionRule("I2 != 'A' AND TEMP_O_1 IS NOT NULL "),
+      RunOnPassProcessor(1000, Id(1040,1),OutputExpression("array(TEMP_O_1, 'a', 'EX_3')")))
+    val rule4 = Rule(Id(101, 2), ExpressionRule("I2 = 'A' AND I3 IN ('a', 'b') AND I4 IS NOT NULL"),
+      RunOnPassProcessor(1000, Id(1040,1),OutputExpression("array(I4, 'b', 'EX_4')")))
+    val ruleSuite2 = RuleSuite(Id(22, 1034), Seq(RuleSet(Id(33, 345), Seq(rule3,rule4))))
+    val schema2=DataType.fromDDL("ARRAY<STRING>")
+    val rer2 :org.apache.spark.sql.Column = ruleEngineRunner(ruleSuite2,schema2,debugMode = false)
+
+    val twoOutDf = oneOutDf.select(col("*"), rer2.alias("together")).select(col("*"), expr(exprString)).
+      withColumn("TEMP_O_2",col("result").getItem(0)).withColumn("REASON_CODE",col("result").getItem(1)).
+      withColumn("RULE_ID",col("result").getItem(2))
+      .drop("together","result")
+    twoOutDf.head() // should work
+
+    val rule5 = Rule(Id(100, 2),
+      ExpressionRule("REASON_CODE IN ('a', 'b')  AND I2 = 'A' AND I3 IN ('a', 'b', 'c', 'd') AND TEMP_O_2 = 'E'"),
+      RunOnPassProcessor(1000, Id(1040,1),OutputExpression("array('N', 'EX_6')")))
+    val rule6 = Rule(Id(101, 2), ExpressionRule("TEMP_O_2 ='UNKNOWN' and I2 = 'A' AND I3 IN ('a', 'b', 'c', 'd')  "),
+      RunOnPassProcessor(1000, Id(1040,1),OutputExpression("array('N', 'EX_7')")))
+    val rule7 = Rule(Id(102, 2), ExpressionRule("TEMP_O_2 ='UNKNOWN'"), RunOnPassProcessor(1000, Id(1040,1),
+      OutputExpression("array('R', 'EX_8')")))
+
+    val ruleSuite3 = RuleSuite(Id(22, 1034), Seq(RuleSet(Id(33, 345), Seq(rule5,rule6,rule7))))
+    val schema3=DataType.fromDDL("ARRAY<STRING>")
+    val rer3 :org.apache.spark.sql.Column = ruleEngineRunner(ruleSuite3,schema3,debugMode = false)
+
+    val threeOutDf = twoOutDf.select(col("*"), rer3.alias("together")).select(col("*"), expr(exprString)).
+      withColumn("TEMP_O_3",col("result").getItem(0)).withColumn("RULE_ID",col("result").getItem(1))
+      .drop("together","result")
+    //threeOutDf.cache()
+
+    // with .cache these work, without they don’t on databricks
+    //threeOutDf.selectExpr("*","TEMP_O_2 IS not NULL as test").filter($"test" === lit("false")).show()
+    //display(threeOutDf.filter("TEMP_O_2 IS NULL"))
+    threeOutDf.filter("TEMP_O_3 IS NULL").head() // doesn't work on 14.3
+  } }
 
 }
+
+case class Compilation_110(STARTER: String, STARTER_FLAG: String, I1: Option[String], I2: String, I3: String, I4: Option[String])
