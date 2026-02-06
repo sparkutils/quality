4e474d925fd4c9240511f08550988f0ac20672b8
Chris Twiner <chris.twiner@gmail.com>
2026-02-06 09:19:25 +0100

    #93 - basis for statistics in udaf

--- src/main/scala/com/sparkutils/quality/impl/SparkHelpers.scala
+++ src/main/scala/com/sparkutils/quality/impl/SparkHelpers.scala
@@ -113,6 +113,23 @@ trait EncodersImplicits extends Serializable {
 
   implicit def ruleFolderResultExpEnc[T: TypedEncoder]: Encoder[RuleFolderResult[T]] = TypedExpressionEncoder[RuleFolderResult[T]]
 
+
+  implicit val ruleStatisticsTypedEnc = TypedEncoder[RuleStatistics]
+
+  implicit val ruleStatisticsTypedExpEnc = TypedExpressionEncoder[RuleStatistics]
+
+  implicit val ruleSetStatisticsTypedEnc = TypedEncoder[RuleSetStatistics]
+
+  implicit val ruleSetStatisticsTypedExpEnc = TypedExpressionEncoder[RuleSetStatistics]
+
+  implicit val ruleSuiteStatisticsTypedEnc = TypedEncoder[RuleSuiteStatistics]
+
+  implicit val ruleSuiteStatisticsTypedExpEnc = TypedExpressionEncoder[RuleSuiteStatistics]
+
+  implicit val ruleSuiteGroupStatisticsTypedEnc = TypedEncoder[RuleSuiteGroupStatistics]
+
+  implicit val ruleSuiteGroupStatisticsTypedExpEnc = TypedExpressionEncoder[RuleSuiteGroupStatistics]
+
 }
 
 object Encoders extends EncodersImplicits {
