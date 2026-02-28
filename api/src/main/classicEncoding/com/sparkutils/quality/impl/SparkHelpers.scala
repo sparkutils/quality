d9a701a23fdbd1df6b6188633d9a00cc060eb1f9
Chris Twiner <chris.twiner@gmail.com>
2026-02-28 10:33:15 +0100

    #104 - docs, extra tests for processor on dq / group results - dbr children pending

--- api/src/main/classicEncoding/com/sparkutils/quality/impl/SparkHelpers.scala
+++ api/src/main/classicEncoding/com/sparkutils/quality/impl/SparkHelpers.scala
@@ -1,6 +1,6 @@
 package com.sparkutils.quality.impl
 
-import com.sparkutils.quality.{GeneralExpressionsResult, RuleEngineResult, RuleFolderResult, RuleResult, RuleSetResult, RuleSetStatistics, RuleStatistics, RuleSuiteGroupStatistics, RuleSuiteResult, RuleSuiteStatistics, VersionedId}
+import com.sparkutils.quality.{GeneralExpressionsResult, RuleEngineResult, RuleFolderResult, RuleResult, RuleSetResult, RuleSetStatistics, RuleStatistics, RuleSuiteGroup, RuleSuiteGroupResults, RuleSuiteGroupStatistics, RuleSuiteResult, RuleSuiteStatistics, VersionedId}
 import org.apache.spark.sql.Encoder
 import shapeless.{::, HList, HNil, LabelledGeneric, Lazy, cachedImplicit}
 import shapeless.ops.hlist.IsHCons
@@ -77,5 +77,9 @@ trait EncodersImplicits extends Serializable {
   implicit val ruleSuiteGroupResultsTypedEnc = TypedEncoder[RuleSuiteGroupResults]
 
   implicit val ruleSuiteGroupResultsTypedExpEnc = TypedExpressionEncoder[RuleSuiteGroupResults]
+
+  implicit val ruleSuiteGroupTypedEnc = TypedEncoder[RuleSuiteGroup]
+
+  implicit val ruleSuiteGroupTypedExpEnc = TypedExpressionEncoder[RuleSuiteGroup]
 
 }
