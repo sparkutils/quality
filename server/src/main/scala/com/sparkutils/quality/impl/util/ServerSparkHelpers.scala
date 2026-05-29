32fb6598beddec2c29e9772e7e40920429f88db4
Chris Twiner <chris.twiner@gmail.com>
2026-05-29 16:31:18 +0200

    #129 - get needed by newinstance to avoid cost

--- server/src/main/scala/com/sparkutils/quality/impl/util/ServerSparkHelpers.scala
+++ server/src/main/scala/com/sparkutils/quality/impl/util/ServerSparkHelpers.scala
@@ -62,13 +62,17 @@ class IntegerArray(val ints: Array[Int]) extends GenericArrayData(null: Array[An
     new IntegerArray(nar)
   }
 
-  override val array: Array[Any] = ints.toArray[Any]
-
   override def isNullAt(ordinal: Int): Boolean = false
 
   def update(i: Int, value: Int): Unit = ints.update(i, value)
 
   override def getInt(ordinal: Int): Int = ints(ordinal)
+
+  override def get(ordinal: Int, dataType: DataType): AnyRef =
+    if (dataType == IntegerType)
+      getInt(ordinal).asInstanceOf[Integer]
+    else
+      ???
 
 }
 
@@ -82,11 +86,15 @@ class LongArray(val longs: Array[Long]) extends GenericArrayData(null: Array[Any
     new LongArray(nar)
   }
 
-  override val array: Array[Any] = longs.toArray[Any]
-
   override def isNullAt(ordinal: Int): Boolean = false
 
   override def getLong(ordinal: Int): Long = longs(ordinal)
+
+  override def get(ordinal: Int, dataType: DataType): AnyRef =
+    if (dataType == LongType)
+      getLong(ordinal).asInstanceOf[java.lang.Long]
+    else
+      ???
 
 }
 
