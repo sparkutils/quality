80fa65f1061e3f73ccd7f1a3d186dbc7f5bea724
Chris Twiner <chris.twiner@gmail.com>
2026-05-30 20:48:43 +0200

    #129 - further optimisations, last change on issue ~cut-release

--- server/src/test/scala/com/sparkutils/qualityTests/classicOnly/BigRules.scala
+++ server/src/test/scala/com/sparkutils/qualityTests/classicOnly/BigRules.scala
@@ -274,7 +274,7 @@ class BigRules extends ClassicSharedTests with BigRulesBase {
     doGrouped129ViaTopLevelBooleanGroupingEmpty(sparkSession)
   } }
 
-  test("1:1 rules only") { // requires a 12gb heap and patience, run takes 5m42s on 32g i9-9900 corsair with 12gb heap, 5.22 ms / row
+  ignore("1:1 rules only") { // requires a 12gb heap and patience, run takes 5m42s on 32g i9-9900 corsair with 12gb heap, 5.22 ms / row
     do1to1RulesOnly(sparkSession)
   }
 
