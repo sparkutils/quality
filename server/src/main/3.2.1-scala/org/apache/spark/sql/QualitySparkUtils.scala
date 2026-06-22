a545e566e683450c75783d8c0f4d1594d1d13a38
Chris Twiner <chris.twiner@gmail.com>
2026-06-22 19:07:43 +0200

    #137 - bump version for test build

--- server/src/main/3.2.1-scala/org/apache/spark/sql/QualitySparkUtils.scala
+++ server/src/main/3.2.1-scala/org/apache/spark/sql/QualitySparkUtils.scala
@@ -82,9 +82,9 @@ object ClassicQualitySparkUtils {
    *
    * @param i
    * @param ctx
-   * @return (parameters for function decleration, parmaters for calling, code that must be before fungroup)
+   * @return (parameters for function declaration, parameters for calling, code that must be before fungroup)
    */
-  def genParamsForNested(ctx: CodegenContext, children: Seq[Expression], additional: Seq[ExprValue]): ParameterInformation = {
+  def genParamsForNested(ctx: CodegenContext, children: Seq[Expression], additional: Seq[VariableValue]): ParameterInformation = {
     val (a, b) = getLocalInputVariableValues(ctx, children, ShimExprUtils.currentSubExprState(ctx))
 
     val p = formatParams(ctx, a.toSeq, additional)
@@ -99,7 +99,7 @@ object ClassicQualitySparkUtils {
    * @param ctx
    * @return (parameters for function declaration, parameters for calling, code that must be before fungroup)
    */
-  def genParams(ctx: CodegenContext, child: Expression, additional: Seq[ExprValue] = Seq.empty): ParameterInformation = {
+  def genParams(ctx: CodegenContext, child: Expression, additional: Seq[VariableValue] = Seq.empty): ParameterInformation = {
     val (a, b) = CodeGenerator.getLocalInputVariableValues(ctx, child, ShimExprUtils.currentSubExprState(ctx))
 
     val p = formatParams(ctx, a.toSeq, additional)
